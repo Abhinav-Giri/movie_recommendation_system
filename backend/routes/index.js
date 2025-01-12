ef4a574db1676a6a4cdd44c81a5ff0362edcec97
Abhinav Giri <iamabhinavgiri@gmail.com>
2025-01-13 00:32:06 +0530

    modified structure of code

--- backend/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const recommendationController = require('../controllers/recommendationController');
-
-router.post('/recommendations', recommendationController.getRecommendation);
-
-module.exports = router;
